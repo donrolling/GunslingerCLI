d9203e00ba6ac7f5f928ceda287621fb87bed16d
Don <donrolling@gmail.com>
2022-05-21 11:36:37 -0500

    fix rename test

--- Tests/TemplateSetTests/SwaggerTests.cs
+++ Tests/TemplateSetTests/SwaggerTests.cs
@@ -25,16 +25,16 @@ namespace Tests.TemplateSetTests
             var generatorService = GetService<IGeneratorService>();
             var result = generatorService.Generate(GetCommandSettings(TestContext, filename));
             var template = generatorService.Context.Templates.First();
-            var badFilePath = Path.Join(generatorService.Context.OutputDirectory, template.OutputRelativePath.Replace("{entityName}", "Item"));
-            var goodFilePath = Path.Join(generatorService.Context.OutputDirectory, template.OutputRelativePath.Replace("{entityName}", "PolicyEntityGraph"));
+            var badFilePath = Path.Join(generatorService.Context.OutputDirectory, template.OutputRelativePath.Replace("{entityName}", "Error"));
+            var goodFilePath = Path.Join(generatorService.Context.OutputDirectory, template.OutputRelativePath.Replace("{entityName}", "RenameError"));
             var badFile = new FileInfo(badFilePath);
             var goodFile = new FileInfo(goodFilePath);
             Assert.IsTrue(result.Success, $"This process should have succeeded. Error: {result.Message}");
             Assert.IsFalse(badFile.Exists, $"{badFilePath} shouldn't exist.");
             Assert.IsTrue(goodFile.Exists, $"{goodFilePath} should exist.");
             var contents = File.ReadAllText(goodFilePath);
-            Assert.IsFalse(contents.Contains("public class Item"), "File should have had its contents replaced to reflect the new name.");
-            Assert.IsTrue(contents.Contains("public class PolicyEntityGraph"), "File should have had its contents replaced to reflect the new name.");
+            Assert.IsFalse(contents.Contains("public class Error"), "File should have had its contents replaced to reflect the new name.");
+            Assert.IsTrue(contents.Contains("public class RenameError"), "File should have had its contents replaced to reflect the new name.");
         }
 
         [TestMethod]
